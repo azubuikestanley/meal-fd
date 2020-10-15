0bcb37e404f1be8c931e7dde0f4c41b1308766a7
A-Stanley <astanley@uplandsoftware.com>
2020-10-15 19:49:11 +0100

    making the appbar title dynamic

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,9 +9,15 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  final List<Widget> _pages = [
-    CategoriesScreen(),
-    FavoritesScreen(),
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': FavoritesScreen(),
+      'title': 'Your Favorites',
+    },
   ];
 
   int _selectedPageIndex = 0;
@@ -25,10 +31,12 @@ class _TabsScreenState extends State<TabsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( 
-        title: Text('Meals'),
+      appBar: AppBar(
+        title: Text(
+          _pages[_selectedPageIndex]['title'],
+        ),
       ),
-      body: _pages[_selectedPageIndex],
+      body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
